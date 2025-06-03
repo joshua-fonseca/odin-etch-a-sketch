a1c7a42e3be6adaae60abe3af76e2446749ba287
joshua-fonseca <joshuacfonseca@gmail.com>
2025-06-02 22:21:42 -0400

    added new grid button that deletes previous grid and prompts for a new grid of n size

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ const container = document.querySelector(".container")
 
 let n = 16;
 
-for(let i=0; i<n**2; i++) {
-  const square = document.createElement("div");
+let generateGrid = (n) => {
+  for(let i=0; i<n**2; i++) {
+    const square = document.createElement("div");
 
-  square.style.width = `${100 / n}%`;
-  square.style.aspectRatio = "1 / 1";
-  square.classList.add("square");
+    square.style.width = `${100 / n}%`;
+    square.style.aspectRatio = "1 / 1";
+    square.classList.add("square");
 
-  square.addEventListener("mouseover", (event) => {
-    square.style.backgroundColor = "black";
-  });
+    square.addEventListener("mouseover", (event) => {
+      square.style.backgroundColor = "black";
+    });
 
-  container.appendChild(square);
+    container.appendChild(square);
+  }
 }
+
+let clearGrid = () => {
+  while(container.firstChild) {
+    container.removeChild(container.firstChild)
+  }
+}
+
+generateGrid(n);
+
+// button
+const newGridButton = document.querySelector("#config");
+
+newGridButton.addEventListener("click", () => {
+   n = parseInt(prompt("Number of square per side for the new grid:"))
+   clearGrid();
+   generateGrid(n);
+});
