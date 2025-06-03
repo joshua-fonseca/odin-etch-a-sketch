a4bb0c7f46f2c586f097e1e83a591078f653e078
joshua-fonseca <joshuacfonseca@gmail.com>
2025-06-02 22:04:07 -0400

    added hover effect so that the grid divs change colour when the mouse passes over them

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ let n = 16;
 
 for(let i=0; i<n**2; i++) {
   const square = document.createElement("div");
+
   square.style.width = `${100 / n}%`;
   square.style.aspectRatio = "1 / 1";
   square.classList.add("square");
+
+  square.addEventListener("mouseover", (event) => {
+    square.style.backgroundColor = "black";
+  });
+
   container.appendChild(square);
 }
